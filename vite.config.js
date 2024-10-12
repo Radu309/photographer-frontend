fbf81f92ef024c9b1a4149c62314986a7577459f
Radu309 <radu.sabin309@gmail.com>
2024-10-12 15:50:22 +0300

    demo deploy

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/home',
-  server: {
-    headers: {
-      'Cache-Control': 'no-store',
-    },
-  },
+  base: '/photographer-frontend/',
 })
