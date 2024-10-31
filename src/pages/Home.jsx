bda48ddafc103e986645497129afa108b8c63628
Radu309 <radu.sabin309@gmail.com>
2024-10-31 18:32:57 +0200

    2/4 blocks ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,12 @@ import wedding2 from '/src/images/wedding2.webp';
 import wedding3 from '/src/images/wedding3.webp';
 import wedding4 from '/src/images/wedding4.webp';
 import logoPhotographer from '/src/icons/jpg/photographer-logo.jpg';
+import rusinaruProfile from '/src/images/rusinaru-profile.jfif';
 import {useState} from "react";
 import "/src/pages/styles/Home.css"
 import "/src/pages/styles/Block-1.css"
+import "/src/pages/styles/Block-2.css"
+import "/src/pages/styles/Block-3.css"
 
 const images = [
     wedding1,
@@ -68,31 +71,24 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                <div className="block-2">
+                <div className="block-3">
                     <div className="container">
-                        <div className="text-left">
-                            Momente reale, capturate cu sinceritate. Fiecare imagine spune o poveste unică și autentică.
+                        <div className="profile-image">
+                            <img src={rusinaruProfile} alt="Profile picture"/>
                         </div>
-                        <div className="vertical-bar"/>
-                        <div className="img-mid">
-                            <img src={logoPhotographer} alt="Logo photographer"/>
-                        </div>
-                        <div className="vertical-bar"/>
-                        <div className="text-right">
-                            Planul tău este să te bucuri de fiecare clipă, iar scopul meu este să transform acele clipe
-                            în amintiri de neuitat
+                        <div className="description-block">
+                            <div className="description-container">
+                                Description
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="block-3">
+                    <div className="block-4">
 
-                </div>
-                <div className="block-4">
-
+                    </div>
                 </div>
             </div>
         </section>
-    );
+);
 }
 
 export default Home;
